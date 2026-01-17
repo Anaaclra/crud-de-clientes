3dfa7d6edb1eb6b5f3a2d4e0ae8ff4a84b32af58
Ana Clara <anaclaraa02pereira@gmail.com>
2026-01-17 00:22:05 -0300

    Primeiro commit do projeto CRUD de clientes

---
+++ src/main/java/com/devsuperior/aula/entities/Client.java
@@ -0,0 +1,20 @@
+package com.devsuperior.aula.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String cpf;
+    private double income;
+    private LocalDate birthDate;
+    private Integer children;
+}
